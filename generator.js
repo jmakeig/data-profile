554aa7000af21e67e4ed796909bf5b5e827ed521
Justin Makeig <jmakeig@marklogic.com>
2018-03-18 07:23:34 -0700

    Generates random notes

--- generator.js
+++ generator.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 
-const f = require('faker');
+const f = require("faker");
+
+/**
+ * Generate an array from random length between `min` and `max`, calling `fct` 
+ * to populate each item.
+ * 
+ * TODO: Support more than just uniform distribution
+ * 
+ * @param {function} fct 
+ * @param {number} min = 0
+ * @param {number} max = 10
+ */
+function several(fct, min = 0, max = 10) {
+  const num = Math.floor(Math.random() * max) + Math.floor(min);
+  const coll = [];
+  for (let i = 0; i < num; i++) {
+    coll.push(fct());
+  }
+  return coll;
+}
 
 const customers = [];
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10; i++) {
   customers.push({
     id: f.random.uuid(),
     name: { first: f.name.firstName(), last: f.name.lastName() },
@@ -12,6 +31,7 @@ for (let i = 0; i < 1000; i++) {
       city: f.address.city(),
       zip: f.address.zipCode()
     },
+    notes: several(f.lorem.sentence, 0, 5),
     verified: f.random.boolean()
   });
 }
